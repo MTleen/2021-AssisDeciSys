24975468dc766d00445f9c2ee53a5df33e71761b
MTleen <673719729@qq.com>
2021-11-16 15:48:56 +0800

    修改小程序首页获取历史信息接口为可匿名访问

--- RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
-                .antMatchers("/login", "/register", "/captchaImage", "/wechatLogin").anonymous()
+                .antMatchers("/login", "/register", "/captchaImage", "/wechatLogin", "/knowledge/knowledge/applist").anonymous()
                 .antMatchers(
                         HttpMethod.GET,
                         "/",
